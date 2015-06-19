2528a6f156891dad7e9fa5e87296e8155c6edbc1
guilherme <gobbi9@live.com>
2015-06-18 23:42:34 -0300

    primeira versão do algoritmo minimax

--- No.js
+++ No.js
@@ -3,6 +3,8 @@ function No(valor){
 	this.nivel = 0;
 	this.pai = null;
 	this.filhos = new Array();
+	this.max = this.valor;
+	this.min = this.valor;
 }
 
 No.prototype.addFilho = function(no) {
@@ -16,11 +18,31 @@ No.prototype.percorrer = function() {
 };
 
 function percorrerRecursivo (no){
-	console.log("Valor: " + no.valor + ", nivel: " + no.nivel);
-
+	//console.log("Valor: " + no.valor + ", nivel: " + no.nivel);
+	console.log("Valor: " + no.valor + ", nivel: " + no.nivel + ", max: " + no.max + ", min: " + no.min);
 	if (no.filhos.length == 0)
 		return;
 
 	for (var i = 0; i < no.filhos.length; i++)
 		percorrerRecursivo(no.filhos[i]);
+}
+
+function minimax (no){
+	for (var i = 0; i < no.filhos.length; i++)
+		minimax(no.filhos[i]);
+
+	if (no.pai != null){
+		if (no.pai.max === null && no.pai.min === null){
+			no.pai.max = no.max;
+			no.pai.min = no.min;
+		}
+		else {
+			if (no.max > no.pai.max ) no.pai.max = no.max;
+			if (no.min < no.pai.min ) no.pai.min = no.min;
+		}
+	}
+
+	if (no.filhos.length == 0)
+		return;
+
 }
